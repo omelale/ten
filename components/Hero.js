bec0c8e7ad754a73dbbafa0c8c74976422bd198c
Ajten <ajtencani@gmail.com>
2022-05-08 00:00:41 +0200

    new components

---
+++ components/Hero.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from 'next/link'
+
+
+function Hero() {
+    return (
+        <section id="hero" className="container flex flex-col-reverse md:flex-row mx-auto items-center px-6 mt-10 space-between-2 md:space-between-0">
+            <div className="flex flex-col mb-32 space-y-12 md:w-1/2">
+                <h1 className="max-w-md text-4xl font-bold text-center md:text-5xl md:text-left">
+                    We help you build your presence online!
+                </h1>
+                <p className="text-base max-w-sm text-center md:text-left text-darkGrayishBlue">
+                    From your website, to your app and social media presence,
+                    let us make your life simpler while you focus on your business.
+                </p>
+                <div className="flex justify-center md:justify-start">
+                    <Link href="/contact">
+                        <a className="p-3 px-6 pt-2 max-w-sm text-white bg-brightRed text-center rounded-full baseline hover:bg-brightRedLight">Contact Us</a>
+                    </Link>
+                </div>
+            </div>
+            <div className="md:w-1/2 ">
+                {/* <Image
+                            alt="Mountains"
+                            src='/illustration-intro.svg'
+                            layout="fixed"
+                            width={400}
+                            height={400}
+                        /> */}
+                <img src="/illustration-intro.svg" alt="" />
+            </div>
+        </section>
+    )
+}
+
+export default Hero
