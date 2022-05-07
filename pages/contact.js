839870e569b966f4f3cbf6959b911c119bfef9de
Ajten <ajtencani@gmail.com>
2022-05-07 21:45:24 +0200

    installed tailwind .css

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function contact() {
   return (
-    <div>Contact us</div>
+      <h1 className="text-3xl font-bold underline">
+          Hello world!
+      </h1>
   )
 }
 
