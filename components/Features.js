1852aae38b7d28ba8943af75067b9383185616c4
Ajten <ajtencani@gmail.com>
2022-05-08 21:57:53 +0200

    removed linter errors

--- components/Features.js
+++ components/Features.js
@@ -6,7 +6,7 @@ function Features() {
             <div className="container flex flex-col md:flex-row px-4 mx-auto mt-10 space-y-12 md:space-y-0">
                 <div className="flex flex-col space-y-12 md:w-1/2">
                     <h2 className="max-w-md text-4xl font-bold text-center md:text-left">
-                        What's different about Ten?
+                        What&lsquo;s different about Ten?
                     </h2>
                     <p className="max-w-sm text-center text-darkGrayishBlue md:text-left">
                         Manage provides all the functionality your team needs, without the
--- components/Testimonials.js
+++ components/Testimonials.js
@@ -7,7 +7,7 @@ function Testimonials() {
         <section id="testimonials">
             <div className="container flex flex-col max-w-6xl px-5 mx-auto mt-32 text-center">
                 <div className="text-4xl font-bold text-center">
-                    What's Different About Manage?
+                    What&lsquo;s Different About Manage?
                 </div>
                 <div className="flex flex-col md:flex-row mt-24 md:space-x-6">
                     <div className="flex flex-col items-center p-6 space-y-6 rounded-lg bg-veryLightGray md:w-1/3">
